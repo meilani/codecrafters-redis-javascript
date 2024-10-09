03355569afd6b6689abb933afb3c941dcf526104
meilani <lauraho2006@gmail.com>
2024-10-08 22:05:26 -0400

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -5,7 +5,14 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
+  
+    connection.on('data', data => {
+        const dataArr = data.toString().split(/[\r\n|\r|\n]+/)
+        console.log(dataArr)
+        if (dataArr.includes('PING')) {
+            connection.write('+PONG\r\n')
+        }
+    })
 });
 
 server.listen(6379, "127.0.0.1");
